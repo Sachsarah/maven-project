c132460ab4502f8d5ed74a67886d85288859d704
Kumar, Sachin <sachin_kumae@optum.com>
2018-05-02 15:34:03 -0400

    Fixed.

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -15,7 +15,7 @@ public class Greeter {
   /**
    * This is comment.
    */
-  public final String greet(String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
